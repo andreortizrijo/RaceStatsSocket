1b733a491daa57da5fb46600b9c5b2c795ea88f7
André Rijo <andreortizrijo@gmail.com>
2021-08-27 23:33:37 +0100

    Is now prepared to recieve the data by type

--- server.py
+++ server.py
@@ -19,14 +19,20 @@ def handle_client(connection, addr):
     while client_connected:
         content_message = ''
         get_lenght = True
-        chunks = []
+        byte_progress = []
+        track_data = []
+        car_data = []
+        time_data = []
+        track_payload = {}
+        car_payload = {}
+        time_payload = {}
         bytes_recd = 0
 
         while True:
             if get_lenght:
                 chunk = connection.recv(HEADER)
 
-            if get_lenght == True and chunk.decode(FORMAT) != '':
+            if get_lenght and chunk.decode(FORMAT) != '':
                 MESSAGE_LENGHT = int(chunk[:HEADER])
                 get_lenght = False
             else:
@@ -36,29 +42,61 @@ def handle_client(connection, addr):
                     if chunk == b'':
                         break
 
-                    chunks.append(chunk)
+                    byte_progress.append(chunk)
                     bytes_recd = bytes_recd + len(chunk)
 
-                content_message = b''.join(chunks)
+                content_message = b''.join(byte_progress)
                 content_message = pickle.loads(content_message)
+
+                print(content_message)
 
                 if content_message == DISCONNECT_MESSAGE:
                     client_connected = False
                     print(f'[{addr}] Client disconnected')
                     return
 
-                if 'SESSION_INFO' in content_message[0]:
-                    upload_data = threading.Thread(target=send_data, args=([content_message]))
-                    upload_data.start()
-                else:
-
+                if 'SESSION_INFO' in content_message:
                     upload_data = threading.Thread(target=send_data, args=([content_message]))
                     upload_data.start()
 
-                    print(f'[{addr}] Data was send to the backend')
+                if 'TRACK_INFO' in content_message:
+                    track_data.append(content_message)
+
+                    if len(track_data) == 60:
+                        track_payload['data'] = json.dumps(track_data)
+                        requests.post('http://127.0.0.1:8000/api-datahandler/upload-track', data=track_payload)
+
+                        track_data = []
+                        track_payload = {}
+
+                        print(f'[{addr}] Track Data was send to the backend')
+
+                if 'CAR_INFO' in content_message:
+                    car_data.append(content_message)
+
+                    if len(car_data) == 60:
+                        car_payload['data'] = json.dumps(car_data)
+                        requests.post('http://127.0.0.1:8000/api-datahandler/upload-car', data=car_payload)
+
+                        car_data = []
+                        car_payload = {}
+
+                        print(f'[{addr}] Car Data was send to the backend')
+
+                if 'TIME_INFO' in content_message:
+                    time_data.append(content_message)
+
+                    if len(time_data) == 60:
+                        time_payload['data'] = json.dumps(time_data)
+                        requests.post('http://127.0.0.1:8000/api-datahandler/upload-time', data=time_payload)
+
+                        time_data = []
+                        time_payload = {}
+
+                        print(f'[{addr}] Time Data was send to the backend')
 
                 get_lenght = True
-                chunks = []
+                byte_progress = []
                 content_message = ''
                 bytes_recd = 0
                 
@@ -67,7 +105,7 @@ def handle_client(connection, addr):
 def send_data(data):
     payload = {}
     payload['data'] = json.dumps(data)
-    requests.post('http://127.0.0.1:8000/api-datahandler/upload', data=payload)
+    requests.post('http://127.0.0.1:8000/api-datahandler/upload-session', data=payload)
 
 def start():
     server.listen(QUEUE)
